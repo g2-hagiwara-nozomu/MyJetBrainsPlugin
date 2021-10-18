12366a53252c1a9886c13804f7e9c4c8c8daced3
GitHub Action <action@github.com>
2021-10-18 04:29:18 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/g2hagiwaranozomu/myjetbrainsplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.g2hagiwaranozomu.myjetbrainsplugin.services
+
+import com.github.g2hagiwaranozomu.myjetbrainsplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/g2hagiwaranozomu/myjetbrainsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/g2hagiwaranozomu/myjetbrainsplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.g2hagiwaranozomu.myjetbrainsplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.g2hagiwaranozomu.myjetbrainsplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
