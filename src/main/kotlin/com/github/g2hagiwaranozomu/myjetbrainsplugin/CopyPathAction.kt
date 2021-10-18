1f6952415243f3052a5f8c85d025117b292aace2
萩原 望 <hagiwara.nozomu@geechs.com>
2021-10-18 13:38:38 +0900

    作成

---
+++ src/main/kotlin/com/github/g2hagiwaranozomu/myjetbrainsplugin/CopyPathAction.kt
@@ -0,0 +1,25 @@
+package com.github.g2hagiwaranozomu.myjetbrainsplugin
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.ide.CopyPasteManager
+import java.awt.datatransfer.StringSelection
+
+
+class CopyPathAction : AnAction() 
+{
+    override fun actionPerformed(e: AnActionEvent) 
+    {
+        val virtualFile = e.getRequiredData(CommonDataKeys.VIRTUAL_FILE)
+        val project = e.getRequiredData(CommonDataKeys.PROJECT)
+
+        // pathの取得
+        val absolutePath = virtualFile.path
+        val basePath = project.basePath ?: ""
+        val pathFromRoot = absolutePath.replace("$basePath/", "")
+
+        // clipboardへのコピー
+        CopyPasteManager.getInstance().setContents(StringSelection(pathFromRoot))
+    }
+}
